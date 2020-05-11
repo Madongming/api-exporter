6616984c4a30d1946ccdc2a8854db9b0e8418695
Madongming <elsove812@gmail.com>
2020-05-11 21:17:17 +0800

    modified:   worker.go

--- worker.go
+++ worker.go
@@ -13,122 +13,118 @@ var cache = cache2go.Cache("api_exporter_cache")
 
 type workerBean struct {
 	Http []httpWorker `yaml:http`
-	Tcp []tcpWorker `yaml:tcp`
-
+	Tcp  []tcpWorker  `yaml:tcp`
 }
 
 type Result struct {
-	Name string
-	Status int
+	Name         string
+	Status       int
 	ResponseTime float64
-	Success int
-	Body string
+	Success      int
+	Body         string
 }
 
-
 type httpWorker struct {
-	Name string `yaml:"name"`
-	Interval int `yaml:"interval"`
-	Url string `yaml:"url"`
-	Method string `yaml:"method"`
-	Header []struct{
-		Key string `yaml:"key"`
+	Name     string `yaml:"name"`
+	Interval int    `yaml:"interval"`
+	Url      string `yaml:"url"`
+	Method   string `yaml:"method"`
+	Header   []struct {
+		Key   string `yaml:"key"`
 		Value string `yaml:"value"`
 	} `yaml:"header"`
-	Body string `yaml:"body"`
+	Body     string `yaml:"body"`
 	CheckStr string `yaml:"check_str"`
 }
 
-func (h *httpWorker) checkResult( result *Result)  {
+func (h *httpWorker) checkResult(result *Result) {
 	if result.Status < 400 {
 		result.Success = 1
-	}else{
+	} else {
 		result.Success = 0
 	}
 	if h.CheckStr != "" {
-		b,err := regexp.MatchString(h.CheckStr,result.Body)
-		if err != nil{
-			log.Error("regexp failed ",result.Body,h.CheckStr)
+		log.Info("check_str:", h.CheckStr, ", response body: ", result.Body)
+		b, err := regexp.MatchString(h.CheckStr, result.Body)
+		if err != nil {
+			log.Error("regexp failed ", result.Body, h.CheckStr)
 			result.Success = 0
-		}else if b {
+		} else if b {
 			result.Success = 1
-		}else{
+		} else {
 			result.Success = 0
 		}
 	}
 }
 
 type tcpWorker struct {
-	Name string `yaml:"name"`
-	Host string `yaml:"host"`
-	Port int `yaml:"port"`
-	Interval int `yaml:"interval"`
+	Name     string `yaml:"name"`
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	Interval int    `yaml:"interval"`
 }
 
-func (t *tcpWorker) checkResult(result *Result)  {
+func (t *tcpWorker) checkResult(result *Result) {
 	result.Success = result.Status
 }
 
-func CallBack(result *Result){
+func CallBack(result *Result) {
 
-	if result != nil{
-		cache.Add(result.Name,time.Minute*60,result)
-	}else{
+	if result != nil {
+		cache.Add(result.Name, time.Minute*60, result)
+	} else {
 		log.Warn("save cache failed, result is null")
 	}
 
-
 }
 
-
-
-func initWorker()(workerBean,error)  {
-  w,err := loadConfig(configFile)
-  if err != nil {
-  	log.Fatal("parse config file error",err)
-  	panic("parse config file error")
-  }
-  for _,h := range w.Http {
+func initWorker() (workerBean, error) {
+	w, err := loadConfig(configFile)
+	if err != nil {
+		log.Fatal("parse config file error", err)
+		panic("parse config file error")
+	}
+	for _, h := range w.Http {
 		if h.Interval == 0 {
 			h.Interval = workerInterval
 		}
 		go func(worker httpWorker) {
 			httpRun(&worker, CallBack)
-			for range time.Tick(time.Duration(worker.Interval) * time.Second ) {
+			for range time.Tick(time.Duration(worker.Interval) * time.Second) {
 				httpRun(&worker, CallBack)
 			}
 		}(h)
-  }
-  for _,t := range w.Tcp{
-	  if t.Interval == 0 {
-		  t.Interval = workerInterval
-	  }
-  		go func(tcpw tcpWorker) {
+	}
+	for _, t := range w.Tcp {
+		if t.Interval == 0 {
+			t.Interval = workerInterval
+		}
+		go func(tcpw tcpWorker) {
 			tcpRun(&tcpw, CallBack)
-			for range time.Tick(time.Duration(tcpw.Interval) * time.Second ) {
+			for range time.Tick(time.Duration(tcpw.Interval) * time.Second) {
 				tcpRun(&tcpw, CallBack)
 			}
 		}(t)
-  }
-  return w,nil
+	}
+	return w, nil
 
 }
 
-func loadConfig(path string)(workerBean,error){
+func loadConfig(path string) (workerBean, error) {
 	w := workerBean{}
 
-	data,err:= ioutil.ReadFile(path)
+	data, err := ioutil.ReadFile(path)
 	//fmt.Print(string(data))
-	if err != nil{
-		log.Error("no found config file ",path,err)
+	if err != nil {
+		log.Error("no found config file ", path, err)
 		panic("config file no found")
 	}
 
-	err = yaml.Unmarshal(data,&w)
+	err = yaml.Unmarshal(data, &w)
 	if err != nil {
-		log.Fatal("parse yml config file error", path,err)
+		log.Fatal("parse yml config file error", path, err)
 		panic("parse yml config file error")
 	}
-	return w,nil
+	return w, nil
 
 }
